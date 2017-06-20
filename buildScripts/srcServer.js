eada07f326fa31b37b42359f6a26b0fa77759baa
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-06-19 17:41:50 -0700

    Started using Babel, converted configuration files to use ES6 and our npm Scripts to use Babel-Node in order to demonstrate and use transpiling down to ES5

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var app = express();
+const app = express();
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-var port = 3000;
+const port = 3000;
 
 app.listen(port, function(err) {
     if(err) {
