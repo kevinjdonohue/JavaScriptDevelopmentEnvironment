ffa511403b85161a33cdfb6da7fa84ce5b3bb3ee
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-06-20 23:27:42 -0700

    Added a testSetup file to configure Mocha, added scripts to the package.json file to execute Mocha, and added a couple of example unit tests - to exercise Mocha and JSDOM

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', function() {
+  it('should pass', function() {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', function() {
+  it('should say hello', function(done) {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello World!!!");
+      done();
+      window.close();
+    });
+  });
+});
